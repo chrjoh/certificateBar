a3b63fd7be29e6f5e2cef3432071b33443c57f0b
chrjoh <christerjoha@gmail.com>
2015-10-28 21:35:40 +0100

    save sign id for each certificate

--- yaml/yamlhandler.go
+++ yaml/yamlhandler.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -36,11 +37,12 @@ type Cert struct {
 	PrivateKey   rsa.PrivateKey
 	CertTemplate x509.Certificate
 	CertBytes    []byte
+	Signers      []string
 }
 
 type Certs struct {
 	Certificates []*Cert `yaml:"certificates"`
-	Chain        map[string][]string
+	certSigners  map[string][]string
 }
 
 func Handler() {
@@ -51,39 +53,45 @@ func Handler() {
 		log.Fatalf("error: %v", err)
 	}
 	test.setupSigner()
-	test.sign()
+	test.signAll()
 	//	fmt.Printf("--- test:\n%v\n\n", test)
 
 }
 func (c *Certs) setupSigner() {
-	c.Chain = make(map[string][]string)
+	c.certSigners = make(map[string][]string)
 	for _, val := range c.Certificates {
 		key := val.CertConfig.Parent
 		id := val.CertConfig.Id
 		if key == id {
 			val.signed = true // perform selfSign
-		} else if c.Chain[key] == nil {
-			c.Chain[key] = []string{id}
+		} else if c.certSigners[key] == nil {
+			c.certSigners[key] = []string{id}
 		} else {
-			c.Chain[key] = append(c.Chain[key], id)
+			c.certSigners[key] = append(c.certSigners[key], id)
 		}
 	}
 }
 
-func (c *Certs) sign() {
+func (c *Certs) signAll() {
 	for {
-		sign := signers(c)
+		sign := findSigners(c)
 		if len(sign) == 0 {
 			break
 		}
 		for _, s := range sign {
-			list := c.Chain[s.CertConfig.Id]
+			id := s.CertConfig.Id
+			list := c.certSigners[id]
 			for _, certId := range list {
-				fmt.Printf("%v is Sign: %s\n", s.CertConfig.Id, certId)
+				fmt.Printf("%v is Sign: %s\n", id, certId)
 				for _, cert := range c.Certificates {
 					if cert.CertConfig.Id == certId {
 						// sign certificate method
 						cert.signed = true
+						if s.Signers == nil {
+							cert.Signers = []string{id}
+						} else {
+							cert.Signers = append(s.Signers, id)
+						}
 						break
 					}
 				}
@@ -91,13 +99,16 @@ func (c *Certs) sign() {
 		}
 	}
 	for _, cert := range c.Certificates {
+		if len(cert.Signers) > 0 {
+			fmt.Printf("Cert: %s, has certificate chain: %v\n", cert.CertConfig.Id, strings.Join(cert.Signers, ", "))
+		}
 		if !cert.signed {
 			fmt.Printf("Failed to sign: %s\n", cert.CertConfig.Id)
 		}
 	}
 }
 
-func signers(c *Certs) []*Cert {
+func findSigners(c *Certs) []*Cert {
 	sign := []*Cert{}
 	for _, val := range c.Certificates {
 		if val.signed && !val.toBeUsed {
